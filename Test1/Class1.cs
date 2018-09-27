1bfb572adaf2377f4c54ffe95f3a18916533a042
Laura Campbell <lcampbell@stackoverflow.com>
2018-09-27 11:43:25 -0400

    built solution

---
+++ Test1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    public class Class1
+    {
+        [TestCaseSource("testcases")]
+        public void foo()
+        {
+
+        }
+
+        public static IEnumerable<TestCaseData> testcases { get; } = new[] { new TestCaseData().SetName("&") };
+    }
+}
